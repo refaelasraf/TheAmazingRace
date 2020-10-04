d04a8fb26454e94241e9ddbb1e8b0582a1f10b40
Refael <Refaelasraf@gmail.com>
2020-10-04 15:58:51 +0300

    CHANGE FUNCTIONS NAME S

--- racemoudle.py
+++ racemoudle.py
@@ -7,7 +7,7 @@ import runner_module
 def start_race(runners: set, race_length: int):
     loop = asyncio.get_event_loop()
     for current_runner in runners:
-        loop.create_task(__take_a_step(current_runner, race_length - current_runner.start_point, loop))
+        loop.create_task(__run(current_runner, race_length - current_runner.start_point, loop))
     pending = asyncio.Task.all_tasks()
     try:
         loop.run_until_complete(asyncio.gather(*pending))
@@ -16,7 +16,7 @@ def start_race(runners: set, race_length: int):
     loop.close()
 
 
-async def __take_a_step(runner: runner_module.Runner, remaining_distance: int, loop: asyncio.AbstractEventLoop):
+async def __run(runner: runner_module.Runner, remaining_distance: int, loop: asyncio.AbstractEventLoop):
     if remaining_distance <= 0:
         print("the winner is " + runner.name)
         await __stop_all_runners()
@@ -25,7 +25,7 @@ async def __take_a_step(runner: runner_module.Runner, remaining_distance: int, l
     runner.step += 1
     if runner.do_after_step is not None:
         await runner.do_after_step(runner)
-    await __take_a_step(runner, remaining_distance - 1, loop)
+    await __run(runner, remaining_distance - 1, loop)
 
 
 async def __stop_all_runners():
