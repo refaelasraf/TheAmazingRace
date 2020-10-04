8ed1f80388d92ee140145e69013aa90938296d98
Refael <Refaelasraf@gmail.com>
2020-10-04 14:11:16 +0300

    do it by step instead of seconds

--- racemoudle.py
+++ racemoudle.py
@@ -7,7 +7,7 @@ import runner
 def start_race(runners: set, race_length: int):
     loop = asyncio.get_event_loop()
     for current_runner in runners:
-        loop.create_task(__run(current_runner, race_length - current_runner.start_point, loop))
+        loop.create_task(__take_a_step(current_runner, race_length - current_runner.start_point, loop))
     pending = asyncio.Task.all_tasks()
     try:
         loop.run_until_complete(asyncio.gather(*pending))
@@ -16,12 +16,12 @@ def start_race(runners: set, race_length: int):
     loop.close()
 
 
-async def __run(r: runner.Runner, remaining_distance: int, loop: asyncio.AbstractEventLoop):
+async def __take_a_step(r: runner.Runner, remaining_distance: int, loop: asyncio.AbstractEventLoop):
     if remaining_distance <= 0:
         print("the winner is " + r.name)
         __stop_all_runners()
-    await asyncio.sleep(1)
-    await __run(r, remaining_distance - r.step_per_sec, loop)
+    await asyncio.sleep(1 / r.step_per_sec)
+    await __take_a_step(r, remaining_distance - 1, loop)
 
 
 def __stop_all_runners():
